f933fd666bdd8366323f31e01ee6612115f760c6
Adam Coddington <me@adamcoddington.net>
2012-07-23 09:21:55 -0700

    Updating trove classifiers.

--- setup.py
+++ setup.py
@@ -8,11 +8,16 @@ setup(
     author='Adam Coddington',
     author_email='me@adamcoddington.net',
     classifiers=[
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Utilities',
+        'Topic :: Communications :: Email',
+        'Topic :: Communications :: Email :: Post-Office',
+        'Topic :: Communications :: Email :: Post-Office :: IMAP',
+        'Topic :: Communications :: Email :: Post-Office :: POP3',
+        'Topic :: Communications :: Email :: Email Clients (MUA)',
     ],
     packages=[
         'django_mailbox',
