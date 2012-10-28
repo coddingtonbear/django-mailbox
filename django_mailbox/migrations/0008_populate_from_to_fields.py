5c619d98b8f2d1c9dae695d4127b32f1d9de7855
Adam Coddington <me@adamcoddington.net>
2012-10-27 18:09:23 -0700

    Adding migrations supporting new model design.

---
+++ django_mailbox/migrations/0008_populate_from_to_fields.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import datetime
+import email
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        for message in orm['django_mailbox.message'].objects.all():
+            msg_object = email.message_from_string(
+                    message.body
+                )
+            message.from_header = msg_object['from']
+            message.to_header = msg_object['to']
+            message.save()
+
+    def backwards(self, orm):
+        raise RuntimeError('Cannot reverse this migration.')
+
+    models = {
+        'django_mailbox.mailbox': {
+            'Meta': {'object_name': 'Mailbox'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'uri': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        'django_mailbox.message': {
+            'Meta': {'object_name': 'Message'},
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'from_header': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_reply_to': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'replies'", 'null': 'True', 'to': "orm['django_mailbox.Message']"}),
+            'mailbox': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'messages'", 'to': "orm['django_mailbox.Mailbox']"}),
+            'message_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'outgoing': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'processed': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'references': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'referenced_by'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['django_mailbox.Message']"}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'to_header': ('django.db.models.fields.TextField', [], {})
+        }
+    }
+
+    complete_apps = ['django_mailbox']
+    symmetrical = True
