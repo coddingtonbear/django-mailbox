fc897d9b0b3771594ddfd0d3d16876fda2df182a
Adam Coddington <me@adamcoddington.net>
2013-06-06 01:21:23 -0700

    Bumping version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ tests_require=[
 
 setup(
     name='django-mailbox',
-    version='1.10',
+    version='1.10.1',
     url='http://bitbucket.org/latestrevision/django-mailbox/',
     description='Import mail from POP3, IMAP, local mailboxes or directly from Postfix or Exim4 into your Django application automatically.',
     author='Adam Coddington',
