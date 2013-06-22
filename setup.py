38b2a077ecafc3372c2c99a0c4a792c98c554dc4
Adam Coddington <me@adamcoddington.net>
2013-06-22 15:46:10 -0700

    Adding misplaced comma.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ tests_require=[
 
 setup(
     name='django-mailbox',
-    version='2.0'
+    version='2.0',
     url='http://bitbucket.org/latestrevision/django-mailbox/',
     description='Import mail from POP3, IMAP, local mailboxes or directly from Postfix or Exim4 into your Django application automatically.',
     author='Adam Coddington',
