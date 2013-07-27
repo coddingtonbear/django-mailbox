e858703642cc770614cce0f9355939c72d9d806c
Adam Coddington <me@adamcoddington.net>
2013-07-26 22:19:45 -0700

    Bumping major revision number given new migration and changes to DB storage of message body.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ tests_require = [
 
 setup(
     name='django-mailbox',
-    version='2.2',
+    version='3.0',
     url='http://bitbucket.org/latestrevision/django-mailbox/',
     description=(
         'Import mail from POP3, IMAP, local mailboxes or directly from '
