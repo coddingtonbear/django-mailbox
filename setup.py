992375fc6bee16a48c83559feac8c084b62807f4
Adam Coddington <adam.coddington@coxinc.com>
2014-08-22 19:55:18 -0700

    Version 3.4.2a1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ gmail_oauth2_require = [
 
 setup(
     name='django-mailbox',
-    version='3.4.1',
+    version='3.4.2a1',
     url='http://github.com/coddingtonbear/django-mailbox/',
     description=(
         'Import mail from POP3, IMAP, local mailboxes or directly from '
