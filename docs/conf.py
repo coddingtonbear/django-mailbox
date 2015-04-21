e6fb54560899298227c3eeaddb765a7228c0f4ec
Adam Coddington <me@adamcoddington.net>
2015-04-20 20:55:16 -0700

    Adding model documentation to docs; fixes #47.

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -240,3 +240,7 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+sys.path.insert(0, os.path.abspath('..'))
+from django.conf import settings
+settings.configure()
---
+++ docs/topics/appendix/instance-documentation.rst
@@ -0,0 +1,23 @@
+Class Documentation
+===================
+
+Mailbox
+-------
+
+.. autoclass:: django_mailbox.models.Mailbox
+   :members:
+   :undoc-members:
+
+Message
+-------
+
+.. autoclass:: django_mailbox.models.Message
+   :members:
+   :undoc-members:
+
+Message Attachment
+------------------
+
+.. autoclass:: django_mailbox.models.MessageAttachment
+   :members:
+   :undoc-members:
