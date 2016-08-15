e8bcd56727199de75a0dcefe7590d3866a14f39d
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-08-16 01:00:52 +0200

    Add tests to update Mailbox.last_polling

--- django_mailbox/tests/test_mailbox.py
+++ django_mailbox/tests/test_mailbox.py
@@ -1,3 +1,5 @@
+import os
+
 from django.test import TestCase
 
 from django_mailbox.models import Mailbox
@@ -18,3 +20,17 @@ class TestMailbox(TestCase):
             expected_protocol,
             actual_protocol,
         )
+
+    def test_last_polling_field_exists(self):
+        mailbox = Mailbox()
+        self.assertTrue(hasattr(mailbox, 'last_polling'))
+
+    def test_get_new_mail_update_last_polling(self):
+        mailbox = Mailbox.objects.create(uri="mbox://" + os.path.join(
+                os.path.dirname(__file__),
+                'messages',
+                'generic_message.eml',
+            ))
+        self.assertEqual(mailbox.last_polling, None)
+        mailbox.get_new_mail()
+        self.assertNotEqual(mailbox.last_polling, None)
