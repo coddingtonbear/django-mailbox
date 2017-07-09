d93f426401d7b1647f4796dca163f6b82044ee13
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-07-09 20:35:12 +0200

    Add support to args in tests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'django_mailbox.tests.settings'
 
 from django.core import management
 
-if sys.argv[-1] == 'test':
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
     management.execute_from_command_line()
     sys.exit()
 
