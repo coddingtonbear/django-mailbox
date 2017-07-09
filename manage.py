7f477585faccf1d26b20f5217f99c279544f2cf2
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-07-09 22:59:41 +0200

    Update manage.py to Python3 compatibility

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
-import sys
+#!/usr/bin/env python
 import os
+import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'django_mailbox.tests.settings'
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-from django.core import management
+    from django.core.management import execute_from_command_line
 
-if len(sys.argv) > 1 and sys.argv[1] == 'test':
-    management.execute_from_command_line()
-    sys.exit()
+    execute_from_command_line(sys.argv)
