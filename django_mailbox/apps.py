faebda7b34d3ddca02888c7579923404d98dce2b
Fleur Dragan <fleur@users.noreply.github.com>
2022-08-16 15:26:29 -0500

    From https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys:
    When defining a model, if no field in a model is defined with primary_key=True an implicit primary key is added. The type of this implicit primary key can now be controlled via the DEFAULT_AUTO_FIELD setting and AppConfig.default_auto_field attribute. No more needing to override primary keys in all models.
    
    Maintaining the historical behavior, the default value for DEFAULT_AUTO_FIELD is AutoField.

--- django_mailbox/apps.py
+++ django_mailbox/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class MailBoxConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'django_mailbox'
     verbose_name = _("Mail Box")
