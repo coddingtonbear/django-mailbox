f531c7865bfcec8d62f01af1a03563dcfd48cb57
Jace Manshadi <jmanshad@protonmail.com>
2023-01-18 15:53:18 -0800

    moving logger lines into handle method as requested

--- django_mailbox/management/commands/getmail.py
+++ django_mailbox/management/commands/getmail.py
@@ -4,10 +4,8 @@ from django.core.management.base import BaseCommand
 
 from django_mailbox.models import Mailbox
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        logging.basicConfig(level=logging.INFO)
         Mailbox.get_new_mail_all_mailboxes(args)
