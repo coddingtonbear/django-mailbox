1e0ed8dc4538425ab522f502ea94f40a22860b68
ptav <7433752+ptav@users.noreply.github.com>
2024-10-03 14:44:15 +0100

    Stop logging level INFO being forced

--- django_mailbox/management/commands/getmail.py
+++ django_mailbox/management/commands/getmail.py
@@ -7,5 +7,4 @@ from django_mailbox.models import Mailbox
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        logging.basicConfig(level=logging.INFO)
         Mailbox.get_new_mail_all_mailboxes(args)
